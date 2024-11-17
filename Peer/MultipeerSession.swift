db60e113a1c3ff14540e92a4d28cc4293bb154bf
Eric Wang <eric.wang@hurricanesoft.com.tw>
2024-11-17 23:18:29 +0800

    新增多點連接功能，更新界面顯示可用和已連接設備，並修正本地網路使用說明

--- Peer/MultipeerSession.swift
+++ Peer/MultipeerSession.swift
@@ -2,21 +2,43 @@ import MultipeerConnectivity
 import os
 import Foundation
 
+/*
+ MultipeerSession.swift
+ 
+ 這個類負責處理設備之間的多點連接功能：
+ - 使用 MultipeerConnectivity 框架實現設備發現和連接
+ - 自動為設備生成唯一標識符和顯示名稱
+ - 管理可用設備列表和已連接設備列表
+ - 處理設備之間的連接狀態變化
+ */
+
 class MultipeerSession: NSObject, ObservableObject {
+    /// 服務類型標識符，用於設備發現
     private let serviceType = "peer-example"
+    
+    /// 當前設備的唯一標識符
     let myPeerId: MCPeerID
+    
+    /// 管理點對點會話的核心組件
     private var session: MCSession
+    
+    /// 用於廣播本機設備存在的服務
     private var nearbyServiceAdvertiser: MCNearbyServiceAdvertiser
+    
+    /// 用於搜索附近設備的服務
     private var nearbyServiceBrowser: MCNearbyServiceBrowser
     
+    /// 發現但尚未連接的設備列表
     @Published var availablePeers: [MCPeerID] = []
+    
+    /// 已經建立連接的設備列表
     @Published var connectedPeers: [MCPeerID] = []
     
     override init() {
-        // 生成唯一的设备名称
+        // 生成並保存設備的唯一標識符
         let deviceId = UserDefaults.standard.string(forKey: "peer_device_id") ?? UUID().uuidString
         UserDefaults.standard.setValue(deviceId, forKey: "peer_device_id")
-        let deviceName = "设备_\(String(deviceId.prefix(4)))"
+        let deviceName = "設備_\(String(deviceId.prefix(4)))"
         
         myPeerId = MCPeerID(displayName: deviceName)
         session = MCSession(peer: myPeerId, securityIdentity: nil, encryptionPreference: .required)
@@ -29,36 +51,52 @@ class MultipeerSession: NSObject, ObservableObject {
         nearbyServiceAdvertiser.delegate = self
         nearbyServiceBrowser.delegate = self
         
+        // 啟動設備發現服務
         nearbyServiceAdvertiser.startAdvertisingPeer()
         nearbyServiceBrowser.startBrowsingForPeers()
     }
     
+    /// 清理資源，停止設備發現服務
     deinit {
         nearbyServiceAdvertiser.stopAdvertisingPeer()
         nearbyServiceBrowser.stopBrowsingForPeers()
     }
 }
 
+// MARK: - MCSessionDelegate
 extension MultipeerSession: MCSessionDelegate {
+    /// 處理設備連接狀態變化
     func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
         DispatchQueue.main.async {
             self.connectedPeers = session.connectedPeers
         }
     }
     
+    /// 接收其他設備發送的數據
     func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {}
+    
+    /// 接收數據流
     func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {}
+    
+    /// 開始接收資源
     func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {}
+    
+    /// 完成資源接收
     func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {}
 }
 
+// MARK: - MCNearbyServiceAdvertiserDelegate
 extension MultipeerSession: MCNearbyServiceAdvertiserDelegate {
+    /// 處理來自其他設備的連接邀請
     func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didReceiveInvitationFromPeer peerID: MCPeerID, withContext context: Data?, invitationHandler: @escaping (Bool, MCSession?) -> Void) {
+        // 自動接受所有連接請求
         invitationHandler(true, session)
     }
 }
 
+// MARK: - MCNearbyServiceBrowserDelegate
 extension MultipeerSession: MCNearbyServiceBrowserDelegate {
+    /// 發現新的設備時調用
     func browser(_ browser: MCNearbyServiceBrowser, foundPeer peerID: MCPeerID, withDiscoveryInfo info: [String : String]?) {
         DispatchQueue.main.async {
             if !self.availablePeers.contains(peerID) {
@@ -67,6 +105,7 @@ extension MultipeerSession: MCNearbyServiceBrowserDelegate {
         }
     }
     
+    /// 設備離開範圍或不可用時調用
     func browser(_ browser: MCNearbyServiceBrowser, lostPeer peerID: MCPeerID) {
         DispatchQueue.main.async {
             self.availablePeers.removeAll(where: { $0 == peerID })
--- Peer/PeerApp.swift
+++ Peer/PeerApp.swift
@@ -2,15 +2,20 @@
 //  PeerApp.swift
 //  Peer
 //
-//  Created by EricWang on 2024/11/17.
+//  應用程序的主入口點：
+//  - 使用 SwiftUI 的 App 協議
+//  - 設置應用程序的主視圖結構
+//  - 初始化應用程序的根視圖
 //
 
 import SwiftUI
 
 @main
 struct PeerApp: App {
+    /// 配置應用程序的主場景
     var body: some Scene {
         WindowGroup {
+            // 設置 ContentView 為應用程序的根視圖
             ContentView()
         }
     }
