e95102bf4fe4df3ca27da7eea8532ed9343185f6
Eric Wang <eric.wang@hurricanesoft.com.tw>
2024-11-17 16:19:45 +0800

    Initial Commit

---
+++ Peer/PeerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeerApp.swift
+//  Peer
+//
+//  Created by EricWang on 2024/11/17.
+//
+
+import SwiftUI
+
+@main
+struct PeerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PeerTests/PeerTests.swift
@@ -0,0 +1,17 @@
+//
+//  PeerTests.swift
+//  PeerTests
+//
+//  Created by EricWang on 2024/11/17.
+//
+
+import Testing
+@testable import Peer
+
+struct PeerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
